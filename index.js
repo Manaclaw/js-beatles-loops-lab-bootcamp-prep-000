76992e5519209088e029ab59535fafaed23b80fe
Steve <stevenjameswatkin@gmail.com>
2018-05-01 05:48:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var bandPlays = []
-  for (let i = 0; i > musicians.length; i++) {
+  for (let i = 0; i < musicians.length; i++) {
     bandPlays.push(musicians[1] + "plays" + instruments[1])
     
   }
